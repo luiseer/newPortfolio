94e4520469fd81c3951119b4289ae6fa978491a2
luiseer <85654418+luiseer@users.noreply.github.com>
2022-07-28 08:32:57 -0500

    [FEATURE] add fontawesome in skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -3,6 +3,7 @@ import 'aos/dist/aos.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
   faCss3,
+  faGit,
   faHtml5,
   faJs,
   faNode,
@@ -15,10 +16,10 @@ AOS, init();
 const Skills = () => {
   return (
     <>
-      <h2 className="text-center text-white text-2xl mt-10 px-5 py-3">
+      <h2 className="text-center text-white text-2xl mt-20 px-5 py-3">
         Mis habilidades
       </h2>
-      <div className="flex flex-row flex-wrap justify-center gap-20">
+      <div className="flex flex-row flex-wrap justify-center mt-10 gap-20">
         <div data-aos="fade-up-right">
           <h3>html</h3>
           <FontAwesomeIcon
@@ -33,21 +34,21 @@ const Skills = () => {
             icon={faCss3}
           />
         </div>
-        <div>
+        <div data-aos="zoom-in">
           <h3>JavaScript</h3>
           <FontAwesomeIcon
             className="text-blue-color font-extrabold text-9xl"
             icon={faJs}
           />
         </div>
-        <div>
+        <div data-aos="zoom-in">
           <h3>React</h3>
           <FontAwesomeIcon
             className="text-blue-color font-extrabold text-9xl"
             icon={faReact}
           />
         </div>
-        <div data-aos="fade-up-left">
+        <div data-aos="zoom-in">
           <h3>NodeJS</h3>
           <FontAwesomeIcon
             className="text-blue-color font-extrabold text-9xl"
@@ -59,6 +60,13 @@ const Skills = () => {
           <FontAwesomeIcon
             className="text-blue-color font-extrabold text-9xl"
             icon={faPython}
+          />
+        </div>
+        <div data-aos="fade-up-left">
+          <h3>Git</h3>
+          <FontAwesomeIcon
+            className="text-blue-color font-extrabold text-9xl"
+            icon={faGit}
           />
         </div>
       </div>
